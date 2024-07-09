db837ce5c2ed08b3ae818e9105ed5180b295f422
Kamonrat Meesak <kamonratmeesak@MacBook-Pro-khxng-Kamonrat.local>
2024-07-09 09:54:57 +0700

    first commit

--- server/src/app.js
+++ server/src/app.js
@@ -1,1 +1,12 @@
-console.log('hello nodejs world');
+let express = require('express');
+const app = express();
+
+app.get('/status', function(req, res) {
+    res.send('Hello nodejs server');
+})
+
+let port = 8080
+
+app.listen(port, function() {
+    console.log('server running on http://localhost:' + port)
+});
